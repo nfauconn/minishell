7bcf9d49bc9b9762ac3cc77e4f61096b8a1497a5
Noemi FAUCONNIER <nfauconn@e3r7p4.clusters.42paris.fr>
2022-05-02 18:48:28 +0200

    k

--- test.c
+++ test.c
@@ -1,26 +1,51 @@
 #include"minishell.h"
 
-void	clean_exit(int sig_num, siginfo_t *info, void *context)
-{
-	(void)info;
-	(void)context;
-	(void)sig_num;
-	exit(1);
-}
-
 void	new_prompt(int sig_num, siginfo_t *info, void *context)
 {
 	(void)info;
 	(void)context;
 	if (sig_num == SIGINT)
-		printf("%s\n", readline("minishell> "));
+	{
+		printf("\n");
+		readline("minishell> ");
+	}
+/* 	if (sig_num == SIGQUIT)
+		rl_clear_history(); */
 }
 
-int	main()
+void	set_signals(void)
 {
 	struct sigaction	s_act;
 
 	s_act.sa_sigaction = new_prompt;
+	s_act.sa_flags = SA_SIGINFO;
+	sigemptyset(&s_act.sa_mask);
 	sigaction(SIGINT, &s_act, NULL);
-	printf("%s\n", readline("minishell> "));
+	sigaction(SIGQUIT, &s_act, NULL);	
+}
+
+static char	*line_read = (char *)NULL;
+
+void	get_input()
+{
+	if (line_read)
+	{
+		free(line_read);
+		line_read = NULL;
+	}
+	line_read = readline("minishell> ");
+	if (line_read && *line_read)
+		add_history(line_read);
+//	return (line_read);
+}
+
+int	main()
+{
+//	char *input;
+
+	while (1)
+	{
+		set_signals();
+		get_input();
+	}
 }
