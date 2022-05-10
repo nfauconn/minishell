eeaac827476ba3f1f5aeab503fbee5f1472278fb
user42 <nfauconn@student.42.fr>
2022-05-10 17:38:59 +0200

    dirt

--- parse.c
+++ parse.c
@@ -1,10 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/10 16:54:56 by user42            #+#    #+#             */
+/*   Updated: 2022/05/10 17:15:09 by user42           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 
 #include "minishell.h"
 
-static void	go_through_whitespaces(char **s)
+static int	ft_is_quote(int c)
 {
-	while (ft_is_whitespace(**s))
-		(*s)++;
+	return (c == QUOTE || c == DOUBLE_QUOTE);
 }
 
 static int	is_ending_quote(char **s, char c)
@@ -13,6 +24,7 @@ static int	is_ending_quote(char **s, char c)
 	{
 		if (**s == c)
 			return (1);
+		(*s)++;
 	}
 	return (0);
 }
@@ -23,30 +35,35 @@ static void	find_end_token(char **s)
 	char	c;
 
 	c = **s;
-	if (c == '\"' || c == '\'')
+	if (ft_is_quote(c))
 	{
 		(*s)++;
 		tmp = (*s);
 		if (is_ending_quote(s, c))
-			return ;
-		else
 		{
-			(*s) = tmp;
-			while (ft_isprint(**s) && !ft_is_whitespace(**s) && (**s) != '\'' && (**s) != '\"')
-				(*s)++;
+			(*s)++;
+			return ;
 		}
+		else
+			(*s) = tmp;
 	}
+	while (ft_isprint(**s) && !ft_is_whitespace(**s) && (**s) != '\'' && (**s) != '\"')
+		(*s)++;
 }
 
 static int	word_count(char *s)
 {
-	int	count;
+	int		count;
+	char	*tmp;
 
 	count = 0;
 	while (*s)
 	{
-		count++;
-		go_through_whitespaces(&s);
+		while (ft_is_whitespace(*s))
+			s++;
+		tmp = s;
+		if (tmp)
+			count++;
 		find_end_token(&s);
 	}
 	return (count);
@@ -65,41 +82,15 @@ char	**tokenized_cmd(t_input *input, char *cmd)
 	i = 0;
   	while (i < size)
 	{
-		go_through_whitespaces(&cmd);
+		while (ft_is_whitespace(*cmd))
+			cmd++;
 		tmp = cmd;
 		find_end_token(&cmd);
+		if (ft_is_quote(*cmd))
+			cmd++;
 		tokenized[i] = ft_substr(tmp, 0, cmd - tmp);
 		i++;
 	}
 	tokenized[size] = NULL;
 	return (tokenized);
 }
-
-/* static void	go_forward_whitespaces(char **line)
-{
-	while (**line == 32 || (**line >= 9 && **line <= 13))
-		(*line)++;
-}
-
-void	parse_line(t_input *input, char *line)
-{
-	char *tmp;
-
-	go_forward_whitespaces(&line);
-	if (*line == '<')
-	{
-		line++;
-		input->is_stdin_redir = 1;
-		go_forward_whitespaces(&line);
-		tmp = line;
-		while (ft_isprint(*line))
-			line++;
-		if ((*tmp == QUOTE && *(line - 1) == QUOTE)
-			|| (*tmp == DOUBLE_QUOTE && *(line - 1) == DOUBLE_QUOTE))
-			input->stdin_file = ft_substr(tmp, 1, line - 1 - tmp - 1);
-		else
-			input->stdin_file = ft_substr(tmp, 0, line - tmp);
-	}
-	go_forward_whitespaces(&line);
-}
- */
