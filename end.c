e24dd1edc698456e7f7b9883f9263d338aa7a552
Noemi FAUCONNIER <nfauconn@e3r7p1.clusters.42paris.fr>
2022-05-11 19:36:17 +0200

    k

--- end.c
+++ end.c
@@ -6,30 +6,16 @@
 /*   By: nfauconn <nfauconn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 16:09:54 by nfauconn          #+#    #+#             */
-/*   Updated: 2022/05/11 17:20:25 by nfauconn         ###   ########.fr       */
+/*   Updated: 2022/05/11 19:21:20 by nfauconn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-static void	free_list(t_list *list)
-{
-	t_list	*tmp;
-	t_list	*to_free;
-
-	while (tmp->next != NULL)
-	{
-		to_free = tmp;
-		tmp = tmp->next;
-		free(to_free);
-	}
-	free(tmp);
-}
 
 void	end(t_input *input)
 {
 	if (input->line_read)
 		free(input->line_read);
 	if (input->token_line)
-		free_list(input->token_line);
+		ft_lstclear(&input->token_line, free);
 }
--- tokenizer.c
+++ tokenizer.c
@@ -6,11 +6,69 @@
 /*   By: nfauconn <nfauconn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 16:59:04 by nfauconn          #+#    #+#             */
-/*   Updated: 2022/05/11 18:07:48 by nfauconn         ###   ########.fr       */
+/*   Updated: 2022/05/11 19:36:11 by nfauconn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static int	ft_is_quote(int c)
+{
+	return (c == QUOTE || c == DOUBLE_QUOTE);
+}
+
+static void	find_end(char **s)
+{
+	while (**s && (**s != '|') && !ft_is_whitespace(**s) && !ft_is_quote(**s))
+		(*s)++;
+	if (ft_is_quote(**s))
+	{
+		/*  tmp = *s (to keep the address of exploring start)
+			is closing quote ? 
+			> yes -> end = after this quote
+			> no -> 
+		*/
+	}
+}
+
+static void	find_start(char **s)
+{
+	while (ft_is_whitespace(**s))
+		(*s)++;
+}
+
+static void	display_list(void *content)
+{
+	printf("|%s| ", (char *)content);
+}
+
+void	tokenize_input(t_input *input, char *line)
+{
+	char	*start;
+	char	*end;
+	char	*token;
+	t_list	*new;
+
+	while (*line)
+	{
+		find_start(&line);
+		if (*line == '\0')
+			break ;
+		start = line;
+		find_end(&line);
+		end = line;
+		token = ft_substr(start, 0, end - start);
+		if (token)
+		{
+			new = ft_lstnew(token);
+			ft_lstadd_back(&input->token_line, new);
+		}
+	}
+	ft_lstiter(input->token_line, display_list);
+	printf("\n");
+}
+
+/* 	
 
 static void	one_token()
 {
@@ -20,11 +78,10 @@ static void	one_token()
 	token = ft_substr(jefheri);
 	new = lst_new(token);
 	ft_lst_add_back(input->token_line, new);
-}
+}	
 
-void	tokenize_input(t_input *input, char *line)
-{
-	
+void	tokenize_input
+{	
 	while (*line)
 	{
 		while (line = whitespace)
@@ -40,3 +97,4 @@ void	tokenize_input(t_input *input, char *line)
 		
 	}
 }
+ */
