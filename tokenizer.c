91d8c70c76456ce403db10724f6f98228996b327
Noemi FAUCONNIER <nfauconn@e3r9p2.clusters.42paris.fr>
2022-05-13 16:29:52 +0200

    k

--- tokenizer.c
+++ tokenizer.c
@@ -6,7 +6,7 @@
 /*   By: nfauconn <nfauconn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 16:59:04 by nfauconn          #+#    #+#             */
-/*   Updated: 2022/05/13 15:46:58 by nfauconn         ###   ########.fr       */
+/*   Updated: 2022/05/13 16:28:33 by nfauconn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,52 +34,62 @@ static int	quote_is_surrounding_next_word(char **s, char quote)
 	return (0);
 }
 
-static char	*find_closing_quote(char *s, char quote)
+static char	*find_closing_quote(char **s, char quote)
 {
 	char	*tmp;
 
-	while (*s)
+	(*s)++;
+	while (**s)
 	{
-		if (*s == quote)
+		if (**s == quote)
 		{
-			tmp = s;
-			if (!quote_is_surrounding_next_word(&s, quote))
-				return (tmp);
+			printf("found quote = %c\n", quote);
+			tmp = *s;
+			if (!quote_is_surrounding_next_word(s, quote))
+			{
+				printf("found closing quote\n");
+				return (*s);
+			}
 		}
-		s++;
+		(*s)++;
 	}
 	return (NULL);
 }
 
-static char	*find_end(char *s)
+static char	*find_end(char **s)
 {
 	char	*tmp;
 
-	while (*s && !ft_is_shell_separator(*s) && !ft_iswhitespace(*s))
+	while (**s && !ft_is_shell_separator(**s) && !ft_iswhitespace(**s))
 	{
-		if (ft_is_quote(*s) && (*(s + 1) != '\0'))
+		if (ft_is_quote(**s) && (*((*s) + 1) != '\0'))
 		{
-			tmp = s;
-			if (quote_is_surrounding_next_word(&s, *s))
-				return (s);
-			s = ++tmp;
-			s = find_closing_quote(s, *s);
-			if (s)
-				return (s);
+			tmp = *s;
+			if (quote_is_surrounding_next_word(s, **s))
+			{
+				(*s)++;
+				return (*s);
+			}
+			*s = tmp;
+			*s = find_closing_quote(s, **s);
+			if (*s)
+			{
+				(*s)++;
+				return (*s);
+			}
 			else
-				s = tmp;
+				*s = tmp;
 		}
-		else
-			s++;
+		(*s)++;
 	}
-	return (s);
+	return (*s);
 }
 
-static char	*find_start(char *s)
+static char	*find_start(char **s)
 {
-	while (ft_iswhitespace(*s))
-		s++;
-	return (s);
+	while (ft_iswhitespace(**s))
+		(*s)++;
+	return (*s);
 }
 
 static void	display_list(void *content)
@@ -97,8 +107,8 @@ void	tokenize_input(t_input *input, char *line)
 	signal_catching_mode(PGM_EXEC);
 	while (*line)
 	{
-		start = find_start(line);
-		end = find_end(line);
+		start = find_start(&line);
+		end = find_end(&line);
 		token = ft_substr(start, 0, end - start);
 		printf("token = |%s|\n", token);
 		if (token)
@@ -106,7 +116,8 @@ void	tokenize_input(t_input *input, char *line)
 			new = ft_lstnew(token);
 			ft_lstadd_back(&input->token_line, new);
 		}
-		line += end - start;
+		line++;
+//		line += end - start;
 	}
 	ft_lstiter(input->token_line, display_list);
 	printf("\n");
