61193c69f28ffa0198e095bfdf37f5042bb31c99
Mathis Dankou <PetitEsprit@users.noreply.github.com>
2022-06-25 18:54:38 +0200

    heredoc but still incomplete

--- srcs/exec/heredoc.c
+++ srcs/exec/heredoc.c
@@ -3,62 +3,84 @@
 /*                                                        :::      ::::::::   */
 /*   heredoc.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mdankou <mdankou@student.42.fr>            +#+  +:+       +#+        */
+/*   By: mdankou < mdankou@student.42.fr >          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/19 12:37:34 by user42            #+#    #+#             */
-/*   Updated: 2022/06/22 17:12:29 by mdankou          ###   ########.fr       */
+/*   Updated: 2022/06/25 18:52:39 by mdankou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "minishell.h"
+#include "minishell.h"
 
-static void	sigTerm(int sig_num)
+//DOIT AJOUTER LE EXPAND 
+
+static void	read_heredoc_nbline(int *nb)
 {
-	if (sig_num == SIGINT)
+	int	fd;
+
+	fd = open("/tmp/.here_doc2", O_RDONLY);
+	if (fd < 0)
+		return ;
+	read(fd, nb, sizeof(int));
+}
+
+static void	write_heredoc_nbline(int nb)
+{
+	int	fd;
+
+	fd = open("/tmp/.here_doc2", O_CREAT | O_TRUNC | O_WRONLY, 0644);
+	if (fd < 0)
+		return ;
+	write(fd, &nb, sizeof(int));
+}
+
+static void	heredoc_job(int *fd, char *delim)
+{
+	int		nbltotal;
+	int		nbl;
+	char	*line;
+
+	signal(SIGINT, SIG_DFL);
+	nbl = 0;
+	nbltotal = 1;
+	read_heredoc_nbline(&nbltotal);
+	*fd = open("/tmp/.here_doc", O_CREAT | O_TRUNC | O_WRONLY, 0644);
+	if (*fd < 0)
+		exit(-1);
+	line = readline("> ");
+	while (++nbl && line && (line[0] == '\n' || ft_strcmp(line, delim)))
 	{
-		write(1, "\n", 1);
-		rl_on_new_line();
-		exit(128 + SIGINT);
+		ft_putendl_fd(line, *fd);
+		free(line);
+		line = readline("> ");
 	}
+	if (!line)
+		ft_printerror("bash: warning: here-document"\
+		" at line %d delimited by end-of-file (wanted `%s')\n", nbltotal, delim);
+	write_heredoc_nbline(nbltotal += nbl);
+	free(line);
+	close(*fd);
+	exit(0);
 }
 
 void	run_heredoc(int *fd, char *delim)
 {
-	char	*line;
-	pid_t	pid;
+	pid_t		pid;
+	int			wstatus;
 
 	pid = fork();
 	if (pid == 0)
 	{
-		signal(SIGINT, sigTerm);
-		*fd = open("/tmp/.here_doc", O_CREAT | O_TRUNC | O_WRONLY, 0644);
-		if (*fd < 0)
-			return ;
-		line = readline("> ");
-		if (!line)
-		{
-			write(1, "\n", 1);
-			return ;
-		}
-		while (line && (line[0] == '\n' || ft_strcmp(line, delim)))
-		{
-			write(*fd, line, ft_strlen(line));
-			write(*fd, "\n", 1);
-			free(line);
-			line = readline("> ");
-			if (!line)
-			{
-				write(1, "\n", 1);
-				break;
-			}
-		}
-		free(line);
-		close(*fd);
-		exit(0);
+		heredoc_job(fd, delim);
 	}
 	signal(SIGINT, SIG_IGN);
-	wait(NULL);
+	wait(&wstatus);
 	signal_catching_mode(INTERACTIVE);
+	if (wstatus != 0)
+	{
+		write(1, "\n", 1);
+		rl_on_new_line();
+	}
 	*fd = open("/tmp/.here_doc", O_RDONLY);
 	if (*fd < 0)
 	{
