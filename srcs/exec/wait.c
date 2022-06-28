d452f868bf8cccce26ff987a228adc37199e69ee
user42 <nfauconn@student.42.fr>
2022-06-28 14:17:25 +0200

    signal handler en cours. fonctionne

--- srcs/exec/wait.c
+++ srcs/exec/wait.c
@@ -6,26 +6,27 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/19 19:07:39 by nfauconn          #+#    #+#             */
-/*   Updated: 2022/06/28 13:37:53 by user42           ###   ########.fr       */
+/*   Updated: 2022/06/28 14:16:49 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	set_last_status(t_sh *sh, int status)
+void	set_last_exit_status(t_sh *sh, int status)
 {
 	sh->last_status = status;
 }
 
-static void	set_last_signal_status(t_sh *sh, int status)
+static void	set_last_signal_status(t_sh *sh, int signal)
 {
-	sh->last_status = 128 + status;
+	sh->last_status = 128 + signal;
 }
 
 int	wait_children(t_sh *sh)
 {
 	pid_t	pid;
 	int		status;
+	int		signal;
 	size_t	i;
 
 	i = 0;
@@ -36,11 +37,14 @@ int	wait_children(t_sh *sh)
 			set_last_exit_status(sh, WEXITSTATUS(status));
 		if (WIFSIGNALED(status))
 		{
-			if (WTERMSIG(status) == SIGSEGV)
+			signal = WTERMSIG(status);
+			if (signal == SIGSEGV)
 				ft_printerror("Segmentation fault (core dumped)\n");
-			else if (WTERMSIG(status) == SIGBUS)
+			else if (signal == SIGBUS)
 				ft_printerror("Bus error (core dumped)\n");
-			set_last_signal_status(sh, status);
+			else if (signal == SIGINT)
+				ft_printerror("\n");
+			set_last_signal_status(sh, signal);
 		}
 		i++;
 	}
