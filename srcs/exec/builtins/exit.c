7676aea23bf2e020251b6c2558654148c27fc8ed
Noemi FAUCONNIER <nfauconn@e1r5p17.clusters.42paris.fr>
2022-07-09 18:39:06 +0200

    by nfauconn at 09/07/2022 18h39 CEST

--- srcs/exec/builtins/exit.c
+++ srcs/exec/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: nfauconn <nfauconn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/09 17:46:29 by nfauconn          #+#    #+#             */
-/*   Updated: 2022/07/09 18:24:32 by nfauconn         ###   ########.fr       */
+/*   Updated: 2022/07/09 18:38:17 by nfauconn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,26 @@
 int		mini_exit(t_sh *sh, t_cmd *cmd)
 {
 	int	i;
+	int	exit_code;
 
 	i = 0;
 	while (cmd->args[i])
 		i++;
-	if (i == 0)
+	if (i == 1)
 		exit_clear(sh, 0);
-	if (i > 1)
+	if (cmd->args[1] && !ft_str_isdigit(cmd->args[1]))
+	{
+		ft_printerror("minish: exit: %s: numeric argument required\n", cmd->args[1]);
+		return (2);
+	}
+	if (i > 2)
 	{
 		error_display("exit", "too many arguments");
 		return (1);
 	}
 	ft_printerror("exit\n");
-	exit_clear(sh, ft_atoi(cmd->args[1]));
+	exit_code = ft_atoi(cmd->args[1]);
+	exit_clear(sh, exit_code);
 	return (0);
 }
 
