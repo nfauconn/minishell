bad7a64eb99208aa99cbb1c1b37592a20d649d72
user42 <nfauconn@student.42.fr>
2022-07-13 15:36:17 +0200

    by user42 at 13/07/2022 15h36 CEST

--- srcs/exec/redir_apply.c
+++ srcs/exec/redir_apply.c
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/08 12:29:45 by user42            #+#    #+#             */
-/*   Updated: 2022/07/13 14:59:10 by user42           ###   ########.fr       */
+/*   Updated: 2022/07/13 15:36:03 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ static void	open_redir(t_sh *sh, t_cmd *cmd, int p[2])
 	if (cmd->infile)
 	{
 		cmd->redir_in = open(cmd->infile, O_RDONLY);
-		if (cmd->redir_in == WRONG_REDIR)
+		if (cmd->redir_in == -1)
 		{
 			error_display(cmd->infile, strerror(errno), 0);
 			close(p[1]);
@@ -45,7 +45,7 @@ static void	open_redir(t_sh *sh, t_cmd *cmd, int p[2])
 			cmd->redir_out = open(cmd->outfile, O_CREAT | O_APPEND | O_WRONLY, 0644);
 		else
 			cmd->redir_out = open(cmd->outfile, O_CREAT | O_TRUNC | O_WRONLY, 0644);
-		if (cmd->redir_out == WRONG_REDIR)
+		if (cmd->redir_out == -1)
 		{
 			error_display(cmd->outfile, strerror(errno), 0);
 			close(p[1]);
