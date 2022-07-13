97a0a3de845b06115851ba7e6b04b0d54022be15
Mathis Dankou <PetitEsprit@users.noreply.github.com>
2022-07-13 18:27:20 +0200

    some fixes for export

--- srcs/exec/builtins/export.c
+++ srcs/exec/builtins/export.c
@@ -3,14 +3,65 @@
 /*                                                        :::      ::::::::   */
 /*   export.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
+/*   By: mdankou < mdankou@student.42.fr >          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 17:53:50 by nfauconn          #+#    #+#             */
-/*   Updated: 2022/07/13 13:14:26 by user42           ###   ########.fr       */
+/*   Updated: 2022/07/13 18:25:19 by mdankou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exec.h"
+
+static void	sort_str_tab(char **tab)
+{
+	char	*tmp;
+	int		size;
+	int		j;
+	int		i;
+
+	size = 0;
+	while (tab[size])
+		size++;
+	if (size < 2)
+		return ;
+	j = -1;
+	while (++j < size)
+	{
+		i = 0;
+		while (++i < size)
+		{
+			if (ft_strcmp(tab[i - 1], tab[i]) > 0)
+			{
+				tmp = tab[i - 1];
+				tab[i - 1] = tab[i];
+				tab[i] = tmp;
+			}
+		}
+	}
+}
+
+static void	print_env_tab(t_list *env)
+{
+	char	**tab;
+	int		j;
+	int		i;
+
+	tab = get_env_tab(env);
+	if (!tab)
+		return ;
+	sort_str_tab(tab);
+	j = 0;
+	while (tab[j])
+	{
+		printf("declare -x ");
+		i = 0;
+		while (tab[j][i] != '=')
+			printf("%c", tab[j][i++]);
+		printf("=\"%s\"\n", ft_strchr(tab[j], '=') + 1);
+		++j;
+	}
+	ft_str_array_free(tab);
+}
 
 void	do_export(t_list **env, char *assign)
 {
@@ -18,7 +69,7 @@ void	do_export(t_list **env, char *assign)
 	char	*str;
 	u_int	var_len;
 
-	var_len = ft_strchr(assign, '=') - assign;
+	var_len = strchr(assign, '=') - assign;
 	if (var_len <= 0)
 		return ;
 	l = (t_list *)(*env);
@@ -37,6 +88,22 @@ void	do_export(t_list **env, char *assign)
 		ft_lstadd_back(env, ft_lstnew(ft_strdup(assign)));
 }
 
+int	is_valid_identifier(char *str)
+{
+	size_t	i;
+
+	if (!ft_isalpha(str[0]) && str[0] != '_')
+		return (0);
+	i = 1;
+	while (str[i] && str[i] != '=')
+	{
+		if (!ft_isalnum(str[i]) && str[i] != '_')
+			return (0);
+		++i;
+	}
+	return (1);
+}
+
 int	mini_export(t_sh *sh, t_cmd *cmd)
 {
 	char	**var_assigns;
@@ -44,11 +111,22 @@ int	mini_export(t_sh *sh, t_cmd *cmd)
 	size_t	j;
 
 	var_assigns = cmd->args;
-	j = 0;
+	j = 1;
 	status = 0;
-	while (var_assigns && var_assigns[++j])
+	if (!var_assigns || !var_assigns[j])
+		print_env_tab(sh->env);
+	j = 1;
+	while (var_assigns && var_assigns[j])
 	{
-		do_export(&sh->env, var_assigns[j]);
+		if (is_valid_identifier(var_assigns[j]))
+			do_export(&sh->env, var_assigns[j]);
+		else
+		{
+			ft_printerror("minish: export: `%s':" \
+				"not a valid identifier\n", var_assigns[j]);
+			status = 1;
+		}
+		++j;
 	}
 	return (status);
 }
