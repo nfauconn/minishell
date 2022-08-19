2bb7395cd5585b349f26c095436ec14f705aed3d
noe <nfauconn@student.42.fr>
2022-08-20 00:29:57 +0200

    fixed the exist status pb

--- srcs/exec/wait.c
+++ srcs/exec/wait.c
@@ -6,7 +6,7 @@
 /*   By: noe <noe@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/19 19:07:39 by nfauconn          #+#    #+#             */
-/*   Updated: 2022/08/20 00:28:04 by noe              ###   ########.fr       */
+/*   Updated: 2022/08/20 00:29:36 by noe              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,7 @@ static void	handle_termsig_display(t_sh *sh, size_t cmd_index, int signal)
 		ft_printerror("\n");
 	else if (signal == SIGQUIT)
 	{
-		if (cmd_index == sh->cmd_nb - 1 || sh->cmd_nb == 1)
+		if (cmd_index == sh->cmd_nb - 1)
 			ft_printerror("Quit (core dumped)\n");
 	}
 }
@@ -70,14 +70,14 @@ void	wait_child(t_sh *sh, size_t cmd_index)
 	}
 	if (WIFEXITED(status))
 	{
-		if (cmd_index == sh->cmd_nb - 1|| sh->cmd_nb == 1)
+		if (cmd_index == sh->cmd_nb - 1)
 			g_last_status = WEXITSTATUS(status);
 	}
 	if (WIFSIGNALED(status))
 	{
 		signal = WTERMSIG(status);
 		handle_termsig_display(sh, cmd_index, signal);
-		if (cmd_index == sh->cmd_nb - 1|| sh->cmd_nb == 1)
+		if (cmd_index == sh->cmd_nb - 1)
 			g_last_status = signal + 128;
 	}
 }
