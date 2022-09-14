9966920c1039fc9bc1adfdc7ca72e0cd9fe6b91a
miaou33 <nfauconn@student.42.fr>
2022-09-14 20:58:11 +0200

    prompt eva01

--- srcs/prompt.c
+++ srcs/prompt.c
@@ -6,7 +6,7 @@
 /*   By: nfauconn <nfauconn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/27 23:20:58 by nfauconn          #+#    #+#             */
-/*   Updated: 2022/09/08 18:02:52 by nfauconn         ###   ########.fr       */
+/*   Updated: 2022/09/14 20:57:27 by nfauconn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ char	*build_prompt(t_list *env)
 	char	*prompt;
 	char	*cwd;
 
-	prompt = ft_strdup("\001\e[1;32m\002MINISH:\001\e[1;33m\002");
+	prompt = ft_strdup("\001\e[1;32m\002MINISH:\001\e[0;39m\002");
 	cwd = getcwd(NULL, 0);
 	if (!cwd)
 		cwd = var_value("PWD", 3, env);
