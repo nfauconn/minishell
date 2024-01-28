07aa02a1751d7fa994496665c40c5646843c6d8d
noe <nfauconn@student.42.fr>
2024-01-28 17:32:58 +0100

    moved stdio below readline header as it is required now

--- Project/includes/extern_libs.h
+++ Project/includes/extern_libs.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   extern_libs.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: noe <noe@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: nfauconn <nfauconn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 20:00:30 by user42            #+#    #+#             */
-/*   Updated: 2022/08/19 16:06:05 by noe              ###   ########.fr       */
+/*   Updated: 2024/01/28 17:32:32 by nfauconn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@
 # include <dirent.h>
 # include <errno.h>
 # include <fcntl.h>
+# include <stdio.h>
 # include <readline/readline.h>
 # include <readline/history.h>
 # include <signal.h>
 # include <stddef.h>
-# include <stdio.h>
 # include <stdint.h>
 # include <string.h>
 # include <sys/stat.h>
